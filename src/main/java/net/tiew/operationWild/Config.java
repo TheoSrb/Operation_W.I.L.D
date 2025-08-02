4317082c6027adc85ad742b79f6e8b063b3ead45
Tiew_37 <143961678+Tiew37@users.noreply.github.com>
2025-08-02 13:43:18 +0200

    starting

---
+++ src/main/java/net/tiew/operationWild/Config.java
@@ -0,0 +1,12 @@
+package net.tiew.operationWild;
+
+import java.util.List;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    static final ModConfigSpec SPEC = BUILDER.build();
+}
