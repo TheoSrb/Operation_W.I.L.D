70eb6c9d23bae652bd3fdcb6c1788bedacb3dd04
Tiew_37 <143961678+Tiew37@users.noreply.github.com>
2025-10-04 09:41:45 +0200

    new kodiak ai

--- src/main/java/net/tiew/operationWild/worldgen/tree/trunk/RedwoodTrunkPlacer.java
+++ src/main/java/net/tiew/operationWild/worldgen/tree/trunk/RedwoodTrunkPlacer.java
@@ -3,8 +3,11 @@ package net.tiew.operationWild.worldgen.tree.trunk;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.block.BeehiveBlock;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
@@ -30,7 +33,7 @@ public class RedwoodTrunkPlacer extends TrunkPlacer {
 
     @Override
     public List<FoliagePlacer.FoliageAttachment> placeTrunk(LevelSimulatedReader level, BiConsumer<BlockPos, BlockState> blockSetter, RandomSource random, int height,
-            BlockPos pos, TreeConfiguration config) {
+                                                            BlockPos pos, TreeConfiguration config) {
 
         for (int i = 0; i < height; i++) {
             placeLog(level, blockSetter, random, pos.above(i), config);
@@ -57,8 +60,38 @@ public class RedwoodTrunkPlacer extends TrunkPlacer {
             createLogPillar(level, blockSetter, random, pos, config, (int) (((double) height / 8) + OWUtils.generateRandomInterval(0, 5)), -2, 0, $$1);
         }
 
+        // Spawn Bee Nest
+        if (random.nextFloat() < 0.05f) {
+            int randomSide = random.nextInt(4);
+            Direction facing;
+            BlockPos beehivePos;
+            int above = random.nextInt(3);
+            int x = random.nextInt(3) - 1;
+            int z = random.nextInt(3) - 1;
 
+            switch (randomSide) {
+                case 0:
+                    facing = Direction.NORTH;
+                    beehivePos = pos.above(above).offset(x, 0, -3);
+                    break;
+                case 1:
+                    facing = Direction.SOUTH;
+                    beehivePos = pos.above(above).offset(x, 0, 3);
+                    break;
+                case 2:
+                    facing = Direction.EAST;
+                    beehivePos = pos.above(above).offset(3, 0, z);
+                    break;
+                default:
+                    facing = Direction.WEST;
+                    beehivePos = pos.above(above).offset(-3, 0, z);
+                    break;
+            }
 
+            BlockState beehiveState = Blocks.BEE_NEST.defaultBlockState()
+                    .setValue(BeehiveBlock.FACING, facing);
+            blockSetter.accept(beehivePos, beehiveState);
+        }
 
         return List.of(new FoliagePlacer.FoliageAttachment(pos.above(height), 0, false));
     }
